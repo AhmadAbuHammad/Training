160657c4e6655fd26026cb6f318f7a4f0db1e305
AhmadAbuHammad <aiabuhammad@outlook.com>
2020-12-22 18:58:19 +0200

    firstMoves

--- Hotflix/Script/ShowHidescript.js
+++ Hotflix/Script/ShowHidescript.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', (event) => {
+    
+    //show menu
     const button = document.querySelector('.headerMenuBtn');
     const menu = document.querySelector('.header__nav');
     button.addEventListener('click', (event) => {
@@ -8,15 +10,29 @@ window.addEventListener('DOMContentLoaded', (event) => {
         menu.style.display = '';
       }
     });
-  
+
+    ///Show search
     const searchBtn = document.querySelector('.headerSearchBtn');
     const closeBtn = document.querySelector('.closeSearch');
     const search = document.querySelector('.header__search');
     searchBtn.addEventListener('click', (event) => {
-      search.style.display = 'flex';
+      if (search.style.display == '') {
+        search.style.display = 'flex';
+      } else {
+        search.style.display = '';
+      }
     });
     closeBtn.addEventListener('click', (event) => {
       search.style.display = '';
     });
+
+    //show play button
+    
+    // button.addEventListener('click', (event) => {
+    //   if (menu.style.display == '') {
+    //     menu.style.display = 'flex';
+    //   } else {
+    //     menu.style.display = '';
+    //   }
+    // });
   });
-  
